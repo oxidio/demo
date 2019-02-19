5b9ed5b6e31b68f4dd0e470676530489d99eda60
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-19 10:59:02 +0100

    load bootstrap.php also if project is installed as composer dependency

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once __DIR__ . '/../source/bootstrap.php';
+call_user_func(function(...$files) {
+    foreach ($files as $file) {
+        if (file_exists($file)) {
+            /** @noinspection PhpIncludeInspection */
+            require_once $file;
+        }
+    }
+}, __DIR__ . '/../source/bootstrap.php', __DIR__ . '/../../../../source/bootstrap.php');
+

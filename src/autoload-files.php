0de55485a63711be0c6a0fe6e5791d1b07954169
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-20 22:00:03 +0100

    project can be bootstrapped without database connection

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -1,5 +1,7 @@
 <?php
 
+use OxidEsales\EshopCommunity\Core;
+
 call_user_func(function(...$files) {
     foreach ($files as $file) {
         if (file_exists($file)) {
@@ -7,5 +9,22 @@ call_user_func(function(...$files) {
             require_once $file;
         }
     }
-}, __DIR__ . '/../source/bootstrap.php', __DIR__ . '/../../../../source/bootstrap.php');
 
+    try {
+        Core\Registry::getConfig()->init();
+    } catch(Core\Exception\DatabaseException $ignore) {
+        foreach (spl_autoload_functions() as $autoload) {
+            if (($autoload[0] ?? null) === Core\Autoload\ModuleAutoload::class) {
+                spl_autoload_unregister($autoload);
+                spl_autoload_register(function(...$args) use($autoload) {
+                    try {
+                        return $autoload(...$args);
+                    } catch(Core\Exception\DatabaseException $e) {
+                        return false;
+                    }
+                }, false);
+                break;
+            }
+        }
+    }
+}, __DIR__ . '/../source/bootstrap.php', __DIR__ . '/../../../../source/bootstrap.php');

4501951a7aef8a031284b4a3be1ed6438fe9c79c
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-25 13:39:35 +0100

    don't call Core\Registry::getConfig()->init during autoload bootstrap

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -1,30 +1,10 @@
 <?php
 
-use OxidEsales\EshopCommunity\Core;
-
 call_user_func(function(...$files) {
     foreach ($files as $file) {
         if (file_exists($file)) {
             /** @noinspection PhpIncludeInspection */
             require_once $file;
-        }
-    }
-
-    try {
-        Core\Registry::getConfig()->init();
-    } catch(Core\Exception\DatabaseException $ignore) {
-        foreach (spl_autoload_functions() as $autoload) {
-            if (($autoload[0] ?? null) === Core\Autoload\ModuleAutoload::class) {
-                spl_autoload_unregister($autoload);
-                spl_autoload_register(function(...$args) use($autoload) {
-                    try {
-                        return $autoload(...$args);
-                    } catch(Core\Exception\DatabaseException $e) {
-                        return false;
-                    }
-                }, false);
-                break;
-            }
         }
     }
 },

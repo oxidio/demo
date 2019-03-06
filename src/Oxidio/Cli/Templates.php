c53a1c04ef2a2db5bba5ba52b726f593e35194e0
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-03-06 23:31:53 +0100

    add filters to the "templates" command

--- src/Oxidio/Cli/Templates.php
+++ src/Oxidio/Cli/Templates.php
@@ -15,18 +15,33 @@ class Templates
      * Analyze template structure (files, blocks, includes)
      *
      * @param IO       $io
+     * @param bool     $filterBlock    Filter templates with blocks
+     * @param bool     $filterInclude   Filter templates with includes
      * @param string   $basePath [%OX_BASE_PATH% . Application/views/flow/tpl/]
      * @param string   $glob [** / *.tpl]
      */
     public function __invoke(
         IO $io,
+        bool $filterBlock,
+        bool $filterInclude,
         string $basePath = OX_BASE_PATH . 'Application/views/flow/tpl/',
         string $glob     = '**/*.tpl'
     ) {
+        $keyValue = function(string $value, string $key) {
+            return "$key ($value)";
+        };
+
         foreach (Template::find($basePath . $glob) as $template) {
-            $io->isVerbose() && $io->title($template->name);
-            $io->isVeryVerbose() && $io->listing($template->blocks);
-            $io->isVeryVerbose() && $io->listing($template->includes);
+            if ($filterBlock && !$template->blocks) {
+                continue;
+            }
+            if ($filterInclude && !$template->includes) {
+                continue;
+            }
+
+            $io->isVerbose() && $io->title("{$template->namespace} ({$template->name})");
+            $io->isVeryVerbose() && $io->listing(fn\traverse($template->blocks, $keyValue));
+            $io->isVeryVerbose() && $io->listing(fn\traverse($template->includes, $keyValue));
         }
     }
 }
